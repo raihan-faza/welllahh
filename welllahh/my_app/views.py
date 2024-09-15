a4eb33bc28ceb042382c35f19a32771aca83a09a
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-09-15 10:50:25 +0700

    feat: add indofood image to its nutritions model endpoint

--- welllahh/my_app/views.py
+++ welllahh/my_app/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
+from keras.applications.imagenet_utils import decode_predictions
+from keras.preprocessing.image import img_to_array, load_img
+from tensorflow.python.keras.backend import set_session
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import default_storage
+from PIL import Image
+import numpy as np
+import tensorflow as tf 
 
 from .models import CustomUser
 
@@ -55,6 +64,54 @@ def login_using_passphrase(request):
 def generate_login_token(request):
     pass
 
+def GetFoodNutrition(foodName: str):
+    foodName = foodName.lower()
+    myFoodNutrition = {}
+    for  idx, food  in settings.INDOFOOD_NUTRITIONS_DF.iterrows():
+        if foodName == food["food_name"]:
+            myFoodNutrition["calories"] = food["calories"]
+            myFoodNutrition["fat"] = food["fat"]
+            myFoodNutrition["carbohydrate"] = food["carbohydrate"]
+            myFoodNutrition["proteins"] = food["proteins"]
+    return myFoodNutrition
+
+
+
+def inference_indofood_image(request):
+    if request.method == "POST":
+        file = request.FILES["imageFile"]
+        file_name = default_storage.save(file.name, file)
+        file_url = default_storage.path(file_name)
+
+    
+        image = load_img(file_url, target_size=(224, 224))
+        numpy_array = img_to_array(image)
+        
+
+        image_batch = np.expand_dims(numpy_array, axis=0)
+        prediction  = settings.INDOFOOD_IMAGE_MODEL.predict(image_batch)
+            
+        sorted_categories = np.argsort(prediction[0])[:-65:-1]
+        labels = settings.INDOFOOD_IMAGE_LABELS
+        prob = np.sort(prediction)
+        cats = []
+        i = 0 
+        prob = prob[0][:-65:-1]
+
+        res = []
+        for predCat in sorted_categories:
+            if prob[i] > 0.15:
+                cats.append(labels[predCat])
+            i +=1
+        if len(cats) == 0:
+            res = [labels[tf.argmax(prediction[0])]]
+        else:
+            res = cats
+        # label = decode_predictions(predictions, top=1)
+        nutritions =  GetFoodNutrition(res[0][1])
+        return render(request, "index.html", {"predictions": res, "nutritions":  nutritions})
+    else:
+        return render(request, "index.html")
 
 def generate_random_string(length=12):
     characters = string.ascii_letters + string.digits
--- welllahh/welllahh/settings.py
+++ welllahh/welllahh/settings.py
@@ -12,6 +12,27 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+import time
+import tensorflow_hub as hub
+from PIL import Image
+import os 
+
+
+
+INDOFOOD_IMAGE_MODEL =  tf.keras.models.load_model('indofood_nutrition_2.keras')
+
+
+INDOFOOD_NUTRITIONS_DF = pd.read_csv("indofood_with_its_nutritions.csv")
+
+
+df2 = pd.read_csv("allfoodName.csv")
+
+INDOFOOD_IMAGE_LABELS = df2.values.tolist()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +76,7 @@ ROOT_URLCONF = "welllahh.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,3 +143,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
